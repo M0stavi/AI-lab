ddbb85fbf42678398d78416adf29ca5bb13ad166
M0stavi <mostavi3673@gmail.com>
2022-07-02 19:05:23 +0600

    UI

--- Project/turtle.py
+++ Project/turtle.py
@@ -10,9 +10,13 @@ Created on Fri Jun  3 11:52:55 2022
 
 import turtle
 
+
+
 screen = turtle.Screen()
 
 screen.bgcolor('lightblue')
+
+x = screen.numinput("Enter distance:",100)
 
 player = turtle.Turtle()
 
@@ -20,25 +24,59 @@ player.color('blue')
 
 player.shape('turtle')
 
+# print(player.size())
+
 ai = player.clone()
 
 ai.color('red')
 
+
+
 player.penup()
 
-player.goto(-250, 100)
+player.goto(-350, 100)
 
 ai.penup()
 
-ai.goto(250, 100)
+ai.goto(350, 100)
 
 true = 1
 
+# x = screen.numinput("Enter distance:",100)
+
+x+=1
+
+stepsize = 700/x
+
+line = turtle.Turtle()
+
+line.penup()
+
+line.goto(-350,80)
+
+base = turtle.Turtle()
+
+base.penup()
+
+base.goto(350, 80)
+
+# line.pendown()
+
+xx = 20
+
+mm = int(x+1)
+
+for i in range(mm):
+    line.pendown()
+    line.forward(xx)
+    line.penup()
+    line.forward(stepsize-xx)
+        
 while true == 1:
 
     steps = screen.numinput("Enter steps:",100)
     
-    player.forward(10*steps)
+    player.forward(stepsize*steps)
     if(player.pos()==ai.pos()):
         screen.title("Player lost the game!")
         break
@@ -51,7 +89,7 @@ while true == 1:
     
     steps_ai = screen.numinput("Enter steps:",100)
     
-    ai.backward(steps_ai*10)
+    ai.backward(steps_ai*stepsize )
     if(player.pos()==ai.pos()):
         screen.title("Player lost the game!")
         break
